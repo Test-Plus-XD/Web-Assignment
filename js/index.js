7229717b37b7ab0a70923f20c6620eca54add905
Test-Plus <123305433+Test-Plus-XD@users.noreply.github.com>
2025-03-19 17:39:49 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -26,6 +26,18 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 });
 
+// Sentry Debug
+Sentry.onLoad(function () {
+    Sentry.init({
+        // Tracing
+        tracesSampleRate: 1.0, // Capture 100% of the transactions
+        // Session Replay
+        replaysSessionSampleRate: 0.5, // This sets the sample rate at 50%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+});
+//myUndefinedFunction();
+
 // Check if logged-in Function
 window.onload = function () {
     // Debugging isLoggedIn variable
